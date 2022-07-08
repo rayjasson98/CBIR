ced95a7d42dd3364ba68cdc0581e7b407544763e
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2022-07-08 15:15:41 +0000

    Add `concatenate_images()` method to query script

--- src/query.py
+++ src/query.py
@@ -1,5 +1,7 @@
 import sys
 
+from PIL import Image
+
 from color import Color
 from daisy import Daisy
 from DB import Database
@@ -13,6 +15,25 @@ from glcm import GLCM
 
 DEPTH = 5
 D_TYPE = 'd1'
+
+
+def concatenate_images(result_images, queried_image_name):
+    images = [Image.open(x) for x in result_images]
+    widths, heights = zip(*(i.size for i in images))
+
+    total_width = sum(widths)
+    max_height = max(heights)
+
+    new_image = Image.new('RGB', (total_width, max_height))
+
+    x_offset = 0
+
+    for image in images:
+        new_image.paste(image, (x_offset, 0))
+        x_offset += image.size[0]
+
+    new_image.save(f'result/retrieval_result/{queried_image_name}')
+
 
 if __name__ == '__main__':
     db = Database()
@@ -48,6 +69,12 @@ if __name__ == '__main__':
     print(f'\n[+] query: {query["img"]}\n')
 
     _, result = infer(query, samples=samples, depth=DEPTH, d_type=D_TYPE)
+    result_images = []
 
     for match in result:
         print(f'{match["img"]}:\t{match["dis"]},\tClass {match["cls"]}')
+        result_images.append(match["img"])
+
+    split_img_path = img_path.split('/')
+    queried_image_name = f'{mthd}/query_{split_img_path[1]}_{split_img_path[2]}'
+    concatenate_images(result_images, queried_image_name)
